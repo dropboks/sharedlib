fd48e3b2004db446738dfb41c2f71f4e7f0730ac
daffadon <daffaputranarendra9@gmail.com>
2025-06-10 09:45:58 +0700

    refactor(middleware): add user_id logging in request

--- middleware/access_logger.go
+++ middleware/access_logger.go
@@ -26,13 +26,24 @@ func AccessLogger(loger zerolog.Logger) gin.HandlerFunc {
 		method := c.Request.Method
 		path := c.Request.URL.Path
 		userAgent := c.Request.UserAgent()
-		userDataHeader := c.Writer.Header().Get("User-Data")
+
 		var userDataID string
+		userDataHeader := c.Request.Header.Get("User-Data")
 		if userDataHeader != "" {
 			var ud userData
 			err := json.Unmarshal([]byte(userDataHeader), &ud)
 			if err == nil {
 				userDataID = ud.UserID
+			}
+		}
+		if userDataID == "" {
+			userDataHeader = c.Writer.Header().Get("User-Data")
+			if userDataHeader != "" {
+				var ud userData
+				err := json.Unmarshal([]byte(userDataHeader), &ud)
+				if err == nil {
+					userDataID = ud.UserID
+				}
 			}
 		}
 
