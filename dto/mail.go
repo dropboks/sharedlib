a37fddbd1cf5deb50111ae6d1c9d7a99921ee176
daffadon <daffaputranarendra9@gmail.com>
2025-06-24 10:16:38 +0700

    refactor(type): remove subject from the struct

--- dto/mail.go
+++ dto/mail.go
@@ -1,7 +1,6 @@
 package dto
 
 type MailNotificationMessage struct {
-	Subject  string   `json:"subject"`
 	Receiver []string `json:"receiver"`
 	MsgType  string   `json:"message_type"`
 	Message  string   `json:"message"`
